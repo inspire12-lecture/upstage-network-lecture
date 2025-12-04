10cc4fe8b54f88bfb3c8bc52fa1d2db457c1b939
inspire12 <ox4443@naver.com>
2025-12-04 14:03:28 +0900

    feat: image 포맷 응답 api 추가
    - api응답 포맷에 따라 image 해석 차이 확인

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
+import os
 
 app = FastAPI()
 
 @app.get("/hello")
 def hello():
     return {"message": "Hello FastAPI!"}
+
+@app.get("/image")
+def get_image():
+    image_path = "image/oh.jpeg"
+    if os.path.exists(image_path):
+        return FileResponse(image_path, media_type="image/jpeg")
+        # return FileResponse(image_path, media_type="text/json")
+    else:
+        return {"error": "Image not found"}
