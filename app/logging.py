6ce45d4abe020f90d051e7b59afaa957e0704cdb
inspire12 <ox4443@naver.com>
2025-12-06 19:31:17 +0900

    feat: logging 기초

---
+++ app/logging.py
@@ -0,0 +1,23 @@
+import logging
+import logging.config
+import os
+
+LOGGING_CONFIG = {
+
+}
+
+
+def init_logging() -> None:
+    """
+    애플리케이션 시작 시 불러서 logging 전체 설정 적용
+    """
+    # logs 디렉토리 없으면 생성
+
+    os.makedirs("logs", exist_ok=True)
+    # 1) 로깅 기본 설정
+    logging.basicConfig(
+        format="%(asctime)s [%(levelname)s] %(filename)s:%(lineno)d - %(message)s",
+        # format="%(asctime)s [%(levelname)s] %(message)s",
+        level=logging.INFO,  # INFO 이상 레벨만 출력
+        filename="logs/upstage-network-info.log"
+    )
--- main.py
+++ main.py
@@ -1,14 +1,20 @@
+import logging
+
 from fastapi import FastAPI
 from fastapi import Request, HTTPException
 from fastapi.responses import JSONResponse
 from app.api.route.user_routers import router as user_router
 from app.exceptions import UserNotFoundError, EmailNotAllowedNameExistsError
+from app.logging import init_logging
 
 app = FastAPI()
-
+init_logging()
+logger = logging.getLogger('error_handler')
 
 @app.exception_handler(EmailNotAllowedNameExistsError)
 async def email_not_allowed_handler(request: Request, exc: EmailNotAllowedNameExistsError):
+    logger.exception("Email Not Allowed exception occurred")
+    # logger.error("Email Not Allowed exception occurred", exc_info=exc)
     return JSONResponse(
         status_code=409,
         content={"error": "Email Not Allowed", "message": str(exc)}
@@ -17,6 +23,8 @@ async def email_not_allowed_handler(request: Request, exc: EmailNotAllowedNameEx
 
 @app.exception_handler(UserNotFoundError)
 async def user_not_found_handler(request: Request, exc: UserNotFoundError):
+    logger.exception("User Not Found exception occurred")
+    # logger.error("User Not Found exception occurred", exc)
     return JSONResponse(
         status_code=404,
         content={"error": "User Not Found", "message": str(exc)}
@@ -25,6 +33,7 @@ async def user_not_found_handler(request: Request, exc: UserNotFoundError):
 
 @app.exception_handler(ValueError)
 async def value_error_handler(request: Request, exc: ValueError):
+    logger.exception("Bad Request exception occurred")
     return JSONResponse(
         status_code=400,
         content={"error": "Bad Request", "message": str(exc)}
@@ -33,6 +42,7 @@ async def value_error_handler(request: Request, exc: ValueError):
 
 @app.exception_handler(HTTPException)
 async def http_exception_handler(request: Request, exc: HTTPException):
+    logger.exception("HTTP exception occurred")
     return JSONResponse(
         status_code=exc.status_code,
         content={"error": "HTTP Exception", "message": exc.detail}
@@ -41,15 +51,18 @@ async def http_exception_handler(request: Request, exc: HTTPException):
 
 @app.exception_handler(Exception)
 async def general_exception_handler(request: Request, exc: Exception):
+    logger.exception("Unhandled exception occurred")
     return JSONResponse(
         status_code=500,
         content={"error": "Internal Server Error", "message": "Something went wrong"}
     )
 
+logger.info("앱 시작")
 
 app.include_router(user_router)
 
 
 @app.get("/hello")
 def hello():
+    logger.info("앱 시작")
     return {"message": "Hello FastAPI!"}
